4a1db5f6cd2eeae9f14e972eeed049b9e487dc09
OxanaDanilova <oxanadanilova11@gmail.com>
2019-03-17 21:17:32 +0100

    feat: Develop function that allows to exchange money

--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    // create object
+    let coins = {
+
+    }
+
+if (currency===0){
+    return {};
+}    
+
+if (currency>10000){
+    return {error: 'You are rich, my friend! We don\'t have so much coins for exchange'};
+} 
+
+if (currency<0){
+    return {};
+}   
+
+console.log(Math.trunc(currency/50));
+
+if (Math.trunc(currency/50)>0) {
+    coins.H=Math.trunc(currency/50);
+    currency=currency-coins.H*50;
+}
+
+if (Math.trunc(currency/25)>0) {
+   coins.Q=Math.trunc(currency/25);
+    currency=currency-coins.Q*25;
+}
+
+if (Math.trunc(currency/10)>0) {
+    coins.D=Math.trunc(currency/10);
+    currency=currency-coins.D*10;
+}
+if (Math.trunc(currency/5)>0) {
+    coins.N=Math.trunc(currency/5);
+    currency=currency-coins.N*5;
+}
+if (currency!==0) {
+    coins.P=currency;}
+
+console.log(coins);
+
+return coins;
+
+
+
 }
